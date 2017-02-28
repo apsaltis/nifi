8ba96041a58f6983d1816fb34b56c2f6b509e53b
fmaritato <frank.maritato@lootcrate.com>
2017-02-28 12:06:22 -0800

    integration test initial check in

---
+++ nifi-nar-bundles/nifi-soap-bundle/nifi-soap-processors/src/test/java/org/apache/nifi/processors/soap/GetSOAPIT.java
@@ -0,0 +1,145 @@
+package org.apache.nifi.processors.soap;
+
+import org.apache.nifi.util.MockFlowFile;
+import org.apache.nifi.util.TestRunner;
+import org.apache.nifi.util.TestRunners;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.List;
+
+import static org.apache.nifi.processors.soap.GetSOAP.REL_SUCCESS;
+
+/**
+ * Integration test that will query live soap services.
+ * Created by frankm on 2/28/17.
+ */
+public class GetSOAPIT {
+
+    private static final Logger logger = LoggerFactory.getLogger(GetSOAPIT.class);
+
+    private TestRunner testRunner;
+
+    @Before
+    public void init() {
+        testRunner = TestRunners.newTestRunner(GetSOAP.class);
+    }
+
+    @After
+    public void after() {
+        testRunner.shutdown();
+    }
+
+    /**
+     * Public service that does not require authentication.
+     */
+    @Test
+    public void testWeather() {
+        testRunner.setProperty(GetSOAP.ENDPOINT_URL,
+                               "https://graphical.weather.gov:443/xml/SOAP_server/ndfdXMLserver.php");
+        testRunner.setProperty(GetSOAP.WSDL_URL, "http://graphical.weather.gov/xml/DWMLgen/wsdl/ndfdXML.wsdl");
+        testRunner.setProperty(GetSOAP.METHOD_NAME, "LatLonListZipCode");
+        testRunner.setProperty(GetSOAP.API_NAMESPACE, "http://graphical.weather.gov/xml/DWMLgen/wsdl/ndfdXML.wsdl");
+        testRunner.setProperty(GetSOAP.SKIP_FIRST_ELEMENT, "true");
+        testRunner.setProperty("zipCodeList", "90720");
+
+        testRunner.enqueue("");
+        testRunner.run();
+
+        testRunner.assertAllFlowFilesTransferred(REL_SUCCESS, 1);
+        List<MockFlowFile> flowFileList = testRunner.getFlowFilesForRelationship(REL_SUCCESS);
+        assert (null != flowFileList);
+        for (MockFlowFile flowFile : flowFileList) {
+            String data = new String(testRunner.getContentAsByteArray(flowFile));
+            logger.info("Data returned: {}", data);
+        }
+    }
+
+    /**
+     * Public service that does not require authentication. This service required the SOAPAction header to be set.
+     * The others didn't seem to require it.
+     */
+    @Test
+    public void testDilbert() {
+        testRunner.setProperty(GetSOAP.ENDPOINT_URL, "http://gcomputer.net/webservices/dilbert.asmx");
+        testRunner.setProperty(GetSOAP.WSDL_URL, "http://www.gcomputer.net/webservices/dilbert.asmx?WSDL");
+        testRunner.setProperty(GetSOAP.METHOD_NAME, "DailyDilbert");
+        testRunner.setProperty(GetSOAP.API_NAMESPACE, "http://gcomputer.net/webservices/");
+        testRunner.setProperty("ADate", "2017-02-01T00:00:00");
+        testRunner.enqueue("");
+        testRunner.run();
+
+        testRunner.assertAllFlowFilesTransferred(REL_SUCCESS, 1);
+        List<MockFlowFile> flowFileList = testRunner.getFlowFilesForRelationship(REL_SUCCESS);
+        assert (null != flowFileList);
+        for (MockFlowFile flowFile : flowFileList) {
+            String data = new String(testRunner.getContentAsByteArray(flowFile));
+            logger.info("Data returned: {}", data);
+        }
+    }
+
+    /**
+     * Public service that does not require authentication.
+     */
+    @Test
+    public void testStockQuote() {
+        testRunner.setProperty(GetSOAP.ENDPOINT_URL, "http://www.webservicex.com/stockquote.asmx");
+        testRunner.setProperty(GetSOAP.WSDL_URL, "http://www.webservicex.com/stockquote.asmx?WSDL");
+        testRunner.setProperty(GetSOAP.METHOD_NAME, "GetQuote");
+        testRunner.setProperty(GetSOAP.API_NAMESPACE, "http://www.webserviceX.NET/");
+        testRunner.setProperty(GetSOAP.SKIP_FIRST_ELEMENT, "true");
+        testRunner.setProperty("symbol", "YHOO"); // Maybe delisted soon? lol
+
+        testRunner.enqueue("");
+        testRunner.run();
+
+        testRunner.assertAllFlowFilesTransferred(REL_SUCCESS, 1);
+        List<MockFlowFile> flowFileList = testRunner.getFlowFilesForRelationship(REL_SUCCESS);
+        assert (null != flowFileList);
+        for (MockFlowFile flowFile : flowFileList) {
+            String data = new String(testRunner.getContentAsByteArray(flowFile));
+            logger.info("Data returned: {}", data);
+        }
+    }
+
+    /**
+     * This is a test of a private service that does require authentication. In order to run this test,
+     * you will need to fill in the endpoint, wsdl, username, password, etc.
+     */
+    @Test
+    public void testServiceWithAuthentication() {
+        testRunner.setProperty(GetSOAP.ENDPOINT_URL, "http://api.someservice.com/v3/SomeService");
+        testRunner.setProperty(GetSOAP.WSDL_URL, "http://api.someservice.com/v3/SomeService?wsdl");
+        testRunner.setProperty(GetSOAP.METHOD_NAME, "getTransactionList");
+        testRunner.setProperty(GetSOAP.API_NAMESPACE, "http://api.someservice.com/");
+        // Testing auth by header and passing username/password as variables.
+        testRunner.setProperty(GetSOAP.AUTH_BY_HEADER, "{\"UserAuthentication\": { " +
+                                                       " \"iId\": \"${Username}\", " +
+                                                       " \"sPassword\": \"${Password}\", " +
+                                                       " \"sType\": \"merchant\" " +
+                                                       "  }" +
+                                                       "}");
+        // Username/Password must be set if referenced above
+        testRunner.setProperty(GetSOAP.USER_NAME, "PUT_YOUR_USER_NAME_HERE");
+        testRunner.setProperty(GetSOAP.PASSWORD, "PUT_YOUR_PASSWORD_HERE");
+        testRunner.setProperty("sDateType", "transaction");
+        // Make sure that dynamic properties can evaluate expression language
+        testRunner.setProperty("dStartDate", "${now():toNumber():minus(86400000):format(\"yyyy-MM-dd'T'HH:mm:ss\")}");
+        testRunner.setProperty("dEndDate", "${now():format(\"yyyy-MM-dd'T'HH:mm:ss\")}");
+
+        testRunner.enqueue("");
+        testRunner.run();
+
+        testRunner.assertAllFlowFilesTransferred(GetSOAP.REL_SUCCESS);
+        List<MockFlowFile> flowFileList = testRunner.getFlowFilesForRelationship(GetSOAP.REL_SUCCESS);
+        Assert.assertNotNull(flowFileList);
+        for (MockFlowFile flowFile : flowFileList) {
+            String data = new String(testRunner.getContentAsByteArray(flowFile));
+            logger.info("DATA" + data);
+        }
+    }
+}
